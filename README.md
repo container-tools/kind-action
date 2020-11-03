805ffa23328866f6ab156a6e3fc49f40223dc586
nicolaferraro <ni.ferraro@gmail.com>
2020-11-03 14:31:22 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# kind-action
+# KinD Action
+
+[![](https://github.com/container-tools/kind-action/workflows/Test/badge.svg?branch=master)](https://github.com/container-tools/kind-action/actions)
+
+A GitHub Action for Kubernetes IN Docker - local clusters for testing Kubernetes using [kubernetes-sigs/kind](https://kind.sigs.k8s.io/).
+
+## Usage
+
+### Pre-requisites
+
+Create a workflow YAML file in your `.github/workflows` directory. An [example workflow](#example-workflow) is available below.
+For more information, reference the GitHub Help Documentation for [Creating a workflow file](https://help.github.com/en/articles/configuring-a-workflow#creating-a-workflow-file).
+
+### Inputs
+
+For more information on inputs, see the [API Documentation](https://developer.github.com/v3/repos/releases/#input)
+
+- `version`: The KinD version to use (default: `v0.8.1`)
+- `config`: The path to the KinD config file
+- `node_image`: The Docker image for the cluster nodes
+- `cluster_name`: The name of the cluster to create (default: `chart-testing`)
+- `wait`: The duration to wait for the control plane to become ready (default: `60s`)
+- `log_level`: The log level for KinD
+- `registry`: Configures a registry on localhost:5000 to be used with KinD (default: `false`)
+
+### Example Workflow
+
+Create a workflow (eg: `.github/workflows/create-cluster.yml`):
+
+```yaml
+name: Create Cluster
+
+on: pull_request
+
+jobs:
+  create-cluster:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Create k8s KinD Cluster
+        uses: container-tools/kind-action@v1.0.0
+```
+
+This uses [@container-tools/kind-action](https://www.github.com/container-tools/kind-action) GitHub Action to spin up a [KinD](https://kind.sigs.k8s.io/) Kubernetes cluster on every Pull Request.
+
+## Credits
+
+This action leverages the good work done by the Helm community on [@helm/kind-action](https://www.github.com/helm/kind-action).
---
+++ kind.sh
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+DEFAULT_KIND_VERSION=v0.8.1
+DEFAULT_CLUSTER_NAME=chart-testing
+KUBECTL_VERSION=v1.18.6
+
+show_help() {
+cat << EOF
+Usage: $(basename "$0") <options>
+
+    -h, --help                              Display help
+    -v, --version                           The kind version to use (default: v0.8.1)"
+    -c, --config                            The path to the kind config file"
+    -i, --node-image                        The Docker image for the cluster nodes"
+    -n, --cluster-name                      The name of the cluster to create (default: chart-testing)"
+    -w, --wait                              The duration to wait for the control plane to become ready (default: 60s)"
+    -l, --log-level                         The log level for kind [panic, fatal, error, warning, info, debug, trace] (default: warning)
+
+EOF
+}
+
+main() {
+    local version="$DEFAULT_KIND_VERSION"
+    local config=
+    local node_image=
+    local cluster_name="$DEFAULT_CLUSTER_NAME"
+    local wait=60s
+    local log_level=
+
+    parse_command_line "$@"
+
+    install_kind
+    install_kubectl
+    create_kind_cluster
+
+}
+
+parse_command_line() {
+    while :; do
+        case "${1:-}" in
+            -h|--help)
+                show_help
+                exit
+                ;;
+            -v|--version)
+                if [[ -n "${2:-}" ]]; then
+                    version="$2"
+                    shift
+                else
+                    echo "ERROR: '-v|--version' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            -c|--config)
+                if [[ -n "${2:-}" ]]; then
+                    config="$2"
+                    shift
+                else
+                    echo "ERROR: '--config' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            -i|--node-image)
+                if [[ -n "${2:-}" ]]; then
+                    node_image="$2"
+                    shift
+                else
+                    echo "ERROR: '-i|--node-image' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            -n|--cluster-name)
+                if [[ -n "${2:-}" ]]; then
+                    cluster_name="$2"
+                    shift
+                else
+                    echo "ERROR: '-n|--cluster-name' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            -w|--wait)
+                if [[ -n "${2:-}" ]]; then
+                    wait="$2"
+                    shift
+                else
+                    echo "ERROR: '--wait' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            -l|--log-level)
+                if [[ -n "${2:-}" ]]; then
+                    log_level="$2"
+                    shift
+                else
+                    echo "ERROR: '--log-level' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            *)
+                break
+                ;;
+        esac
+
+        shift
+    done
+}
+
+install_kind() {
+    echo 'Installing kind...'
+    curl -sSLo kind "https://github.com/kubernetes-sigs/kind/releases/download/$version/kind-linux-amd64"
+    chmod +x kind
+    sudo mv kind /usr/local/bin/kind
+}
+
+install_kubectl() {
+    echo 'Installing kubectl...'
+    curl -sSLO "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/kubectl
+}
+
+create_kind_cluster() {
+    echo 'Creating kind cluster...'
+    local args=(create cluster "--name=$cluster_name" "--wait=$wait")
+
+    if [[ -n "$node_image" ]]; then
+        args+=("--image=$node_image")
+    fi
+
+    if [[ -n "$config" ]]; then
+        args+=("--config=$config")
+    fi
+
+    if [[ -n "$log_level" ]]; then
+        args+=("--loglevel=$log_level")
+    fi
+
+    kind "${args[@]}"
+}
+
+main "$@"
